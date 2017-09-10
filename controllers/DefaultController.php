3aff7f52cb90ff620f6b0c870022c3a4f2c3fe84
andrtechno <andrew.panix@gmail.com>
2017-09-10 06:23:55 +0300

    init

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -5,6 +5,7 @@ namespace panix\mod\admin\controllers;
 use Yii;
 use panix\engine\controllers\AdminController;
 
+use panix\mod\admin\models\Notifactions;
 
 class DefaultController extends AdminController {
 
@@ -14,6 +15,35 @@ class DefaultController extends AdminController {
         return $this->render('index', [
            
         ]);
+    }
+    
+    public function actionAjaxCounters(){
+        
+        $notifactions = Notifactions::find()->read(0)->all();
+        $result = [];
+        $result['count']['cart'] = 5;
+        $result['count']['comments'] = 10;
+        $result['notify'] = [];
+        foreach($notifactions as $notify){
+            $result['notify'][$notify->id]=[
+                'text'=>$notify->text,
+                'type'=>$notify->type
+            ];
+
+        }
+
+        return \yii\helpers\Json::encode($result);
+    }
+    
+    
+    public function actionAjaxReadNotifaction($id){
+        
+        //$notifactions = Notifactions::find()->where(['id'=>$id])->one();
+        $notifactions = Notifactions::findOne($id);
+        $notifactions->is_read = 1;
+        $notifactions->save(false);
+
+        return \yii\helpers\Json::encode(['ok']);
     }
 
 }
