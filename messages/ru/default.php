dfbf22ad5696ca24de7525bdb728d3e7c4c0a7b5
Andrew <andrew.panix@gmail.com>
2019-03-20 21:09:45 +0200

    init

---
+++ messages/ru/default.php
@@ -0,0 +1,25 @@
+<?php
+return [
+    'SYSTEM' => 'Система',
+    'MODULES' => 'Модули',
+    'CREATE_LANG' => 'Добавить язык',
+    'MODULE_NAME' => 'Админ-панель',
+    'SECURITY' => 'Безопасность',
+    'LANGUAGES' => 'Языки',
+    'WIDGETS'=>'Виджеты',
+    'DATABASE' => 'База данных',
+    'OPTIMIZE_DB' => 'Оптимизация базы данных',
+    'OVERHEAD' => 'Накладные расходы',
+    'REPAIR_DB' => 'Ремонт базы данных',
+    'TOTAL_SIZE_DB' => 'Общий размер базы данных',
+    'TOTAL_OVERHEAD' => 'Общие накладные расходы',
+    'DB_OPTIMIZE_REPAIR' => 'Оптимизация или ремонт базы данных',
+    'BACKUP_LIMIT' => 'Лимит на резервное копирование исчерпан {current_size} из {maxsize}',
+    'NO_MODULES_INSTALL' => 'Нет доступных модулей для установки.',
+    'SUCCESS_INSTALL_MODULE' => 'Модуль <b>{name}</b> успешно установлен.',
+    'ERROR_INSTALL_MODULE' => 'Возникла ошибка при установке модуля.',
+    'INSTALLED' => 'Установить',
+    'LIST_MODULES' => 'Список доступных модулей',
+    'INSTALL' => 'Установить ({n})',
+    'WIDGETS_UPDATE'=>'Редактирование виджета'
+];
