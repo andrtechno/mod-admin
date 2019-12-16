41ece62b3f5c269f2640f96f4877921650b5faa5
Andrew <andrew.panix@gmail.com>
2019-12-16 10:59:35 +0200

    init

--- views/admin/settings/_images.php
+++ views/admin/settings/_images.php
@@ -9,9 +9,7 @@
     'asset' => 'Asset (Сохранение изображений в папку assets)',
     'render' => 'Render (Создание изображений на лету)'
 ])->hint($model::t('ATTACHMENT_IMAGE_TYPE_HINT')) ?>
-<?= $form->field($model, 'attachment_wm_path')->fileInput(['accept' => 'image/*']) ?>
-<?= $model->renderWatermarkImage(); ?>
-
+<?= $form->field($model, 'attachment_wm_path')->fileInput(['accept' => 'image/*'])->hint($model->renderWatermarkImage()) ?>
 <?= $form->field($model, 'attachment_wm_corner')->dropDownList($model->getWatermarkCorner()) ?>
 <?= $form->field($model, 'attachment_wm_offsety')->textInput() ?>
 <?= $form->field($model, 'attachment_wm_offsetx')->textInput() ?>
