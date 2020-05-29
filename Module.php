2cddf8351cdf792e813112158b281e69243a6f48
Andrew <andrew.panix@gmail.com>
2020-05-29 14:24:18 +0300

    init

--- Module.php
+++ Module.php
@@ -44,60 +44,61 @@ class Module extends WebModule { // implements BootstrapInterface
                         'label' => Yii::t('app/default', 'SETTINGS'),
                         'url' => ['/admin/app/settings'],
                         'icon' => 'settings',
-                        'visible' => true
+                        'visible' => Yii::$app->user->can('/admin/admin/settings/index') || Yii::$app->user->can('/admin/admin/settings/*')
                     ],
                     [
                         'label' => Yii::t('admin/default', 'LANGUAGES'),
                         'url' => ['/admin/app/languages'],
                         'icon' => 'language',
-                        'visible' => YII_DEBUG
+                        'visible' => Yii::$app->user->can('/admin/admin/languages/index') || Yii::$app->user->can('/admin/admin/languages/*')
                     ],
                     [
                         'label' => Yii::t('admin/default', 'WIDGETS'),
                         'url' => ['/admin/app/widgets'],
                         'icon' => 'chip',
-                        'visible' => YII_DEBUG
+                        'visible' => Yii::$app->user->can('/admin/admin/widgets/index') || Yii::$app->user->can('/admin/admin/widgets/*')
                     ],
                     [
                         'label' => Yii::t('admin/default', 'Mails tpl'),
                         'url' => ['/admin/app/mail-template'],
                         'icon' => 'mail',
-                        'visible' => YII_DEBUG
+                        'visible' => Yii::$app->user->can('/admin/admin/mail-template/index') || Yii::$app->user->can('/admin/admin/mail-template/*')
                     ],
                     [
                         'label' => Yii::t('admin/default', 'DATABASE'),
                         'url' => ['/admin/app/database'],
                         'icon' => 'database',
-                        'visible' => true
+                        'visible' => Yii::$app->user->can('/admin/admin/database/index') || Yii::$app->user->can('/admin/admin/database/*')
                     ],
                     [
                         'label' => Yii::t('admin/default', 'LOGS'),
                         'url' => ['/admin/app/logs'],
                         'icon' => 'log',
+                        'visible' => Yii::$app->user->can('/admin/admin/logs/index') || Yii::$app->user->can('/admin/admin/logs/*')
                     ],
                     [
                         'label' => Yii::t('admin/default', 'MODULES'),
                         'url' => ['/admin/app/modules'],
                         'icon' => 'puzzle',
-                        'visible' => YII_DEBUG
+                        'visible' => Yii::$app->user->can('/admin/admin/modules/index') || Yii::$app->user->can('/admin/admin/modules/*')
                     ],
                     [
                         'label' => Yii::t('admin/default', 'DYNAMIC_FORM'),
                         'url' => ['/admin/app/dynamic-form'],
                         'icon' => 'arrow-right',
-                        'visible' => YII_DEBUG
+                        'visible' => Yii::$app->user->can('/admin/admin/dynamic-form/index') || Yii::$app->user->can('/admin/admin/dynamic-form/*')
                     ],
                     [
                         'label' => Yii::t('admin/default', 'HELP'),
                         'url' => ['/admin/app/help'],
                         'icon' => 'info',
-                        'visible' => true
+                        'visible' => Yii::$app->user->can('/admin/admin/help/index') || Yii::$app->user->can('/admin/admin/help/*')
                     ],
                     [
                         'label' => Yii::t('admin/default', 'TEMPLATE'),
                         'url' => ['/admin/app/template'],
                         'icon' => 'template',
-                        'visible' => YII_DEBUG
+                        'visible' => Yii::$app->user->can('/admin/admin/template/index') || Yii::$app->user->can('/admin/admin/template/*')
                     ],
                 ]
             ]
