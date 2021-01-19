989b1cb3a967c402b8fdc21c68329969eb86e1c3
Andrew <andrew.panix@gmail.com>
2021-01-19 23:12:01 +0200

    init

--- views/admin/settings/_images.php
+++ views/admin/settings/_images.php
@@ -5,10 +5,10 @@
  */
 ?>
 <?= $form->field($model, 'watermark_enable')->checkbox() ?>
-<?= $form->field($model, 'attachment_image_type')->dropDownList([
+<?php /* $form->field($model, 'attachment_image_type')->dropDownList([
     'asset' => 'Asset (Сохранение изображений в папку assets)',
     'render' => 'Render (Создание изображений на лету)'
-])->hint($model::t('ATTACHMENT_IMAGE_TYPE_HINT')) ?>
+])->hint($model::t('ATTACHMENT_IMAGE_TYPE_HINT'));*/ ?>
 <?= $form->field($model, 'attachment_wm_path')->fileInput(['accept' => 'image/*'])->hint($model->renderWatermarkImage()) ?>
 <?= $form->field($model, 'attachment_wm_corner')->dropDownList($model->getWatermarkCorner()) ?>
 <?= $form->field($model, 'attachment_wm_offsety')->textInput() ?>
