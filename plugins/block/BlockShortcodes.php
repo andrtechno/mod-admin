ee7d3f3b315317ed439f4913dc4a90f14cf8ea6c
Andrew <andrew.panix@gmail.com>
2022-07-13 20:04:34 +0300

    Update BlockShortcodes.php

--- plugins/block/BlockShortcodes.php
+++ plugins/block/BlockShortcodes.php
@@ -81,37 +81,4 @@ class BlockShortcodes extends BaseShortcode
             ],
         ];
     }
-
-    public static $config = [
-        'search' => 'Hello, world!',
-        'replace' => 'Hello, Yii!',
-        'color' => '#FFDB51'
-    ];
-
-    /**
-     * @return array
-     */
-    public static function events()
-    {
-        return [
-            Response::class => [
-                Response::EVENT_AFTER_PREPARE => ['hello', self::$config]
-            ]
-        ];
-    }
-
-    /**
-     * @param $event
-     */
-    public static function hello($event)
-    {
-        if (!$content = $event->sender->content) return;
-
-        $search = ArrayHelper::getValue($event->data, 'search', self::$config['search']);
-        $replace = ArrayHelper::getValue($event->data, 'replace', self::$config['replace']);
-        $color = ArrayHelper::getValue($event->data, 'color', self::$config['color']);
-
-        $event->sender->content = str_replace('foo', 'test rep', $content);
-    }
-
 }
